452b45c98dc09824c355180d4c77058024dbb255
manikant kumar <manikant2869@gmail.com>
2023-12-30 13:51:53 +0530

    added first commit

---
+++ controllers/user.js
@@ -0,0 +1,14 @@
+const User = require("../services/user")
+const utils = require("../utils/utils")
+const { TIME } = require("sequelize");
+
+module.exports = {
+    add: async function(req,res){
+        let result = await User.add(req.body);
+        utils.sendResponse(result,req,res);
+    },
+    login: async function(req,res){
+        let result = await User.login(req.body);
+        utils.sendResponse(result,req,res);
+    }
+}
---
+++ services/user.js
@@ -0,0 +1,61 @@
+const initModels = require("../models/init-models");
+const models = initModels(global.sequelize);
+const moment = require("moment");
+const user = models.user;
+
+module.exports = {
+  add: async function (data) {
+    try {
+      let userExist = await user.findOne(data.email);
+      if(userExist){
+        return {
+          message: "You are already registered with us",
+          exist: true
+        }
+      }else{
+      let result = await user.create(data);
+      return {
+        message: "user inserted successfully",
+        exist : false
+      };
+    }
+    } catch (err) {
+      return {
+        err: err,
+      };
+    }
+  },
+  login: async function (data) {
+    try {
+      let finduser;
+      let userdata = await user.findOne({
+        where: { username: data.username }
+      });
+      if (userdata) {
+        let password = userdata.password;
+        if (password === data.password) {
+          finduser = userdata;
+          return {
+            verified: true,
+            finduser:JSON.stringify(userdata),
+            message: "user verified successfully",
+          };
+        } else {
+          return {
+            verified: false,
+            message: "incorrect password",
+          };
+        }
+      } else {
+        return {
+          verified: false,
+          message: "username does not exist",
+        };
+      }
+    } catch (err) {
+      return {
+        err: err,
+      };
+    }
+  },
+};
